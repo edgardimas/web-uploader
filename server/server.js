bd5c11c796003fffc6cc57fcdb69c2264931f5a9
edgardimas <edgar.dimas.ir@gmail.com>
2024-12-29 11:16:04 +0700

    added view feat.

--- server/server.js
+++ server/server.js
@@ -5,12 +5,13 @@ const viewRoutes = require("./src/view/routes");
 const checkForR01Files = require("./src/checker/fileChecker");
 const app = express();
 const port = 3000;
-
+app.set("view engine", "ejs");
+app.set("views", "./src/view"); // Set the views directory to ./src/view
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/orders", orderRoutes);
 app.use("/results", resultRoutes);
-app.use("/views", viewRoutes);
+app.use("/view", viewRoutes);
 
 setInterval(checkForR01Files, 1000);
 
